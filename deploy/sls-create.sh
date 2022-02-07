ea26d36c6352077bf800fc77df303d88cc9ce04f
Gayrat Vlasov <ggvlasov@gmail.com>
2022-02-07 13:33:25 +0300

    Serverless контейнер работает

--- deploy/sls-create.sh
+++ deploy/sls-create.sh
@@ -8,3 +8,7 @@ yc serverless container delete ${CONTAINER_NAME}
 echo "Deploy container"
 export CONTAINER_ID=`yc sls container create --name ${CONTAINER_NAME} --folder-id ${FOLDER_ID} | grep  -E "^id: (.*)$" | sed 's/id: //g'`
 echo "CONTAINER_ID=$CONTAINER_ID" >contid.env
+
+# разрешить публичный вызов
+echo "yc serverless container allow-unauthenticated-invoke --id ${CONTAINER_ID}"
+yc serverless container allow-unauthenticated-invoke --id ${CONTAINER_ID}
---
+++ deploy/sls-deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+export $(grep -v '^#' main.env | xargs -d '\n')
+export $(grep -v '^#' contid.env | xargs -d '\n')
+
+yc sls container revisions deploy \
+	--folder-id ${FOLDER_ID} \
+	--container-id ${CONTAINER_ID} \
+	--memory 512M \
+	--cores 1 \
+	--execution-timeout 5s \
+	--concurrency 4 \
+	--environment AWS_ACCESS_KEY_ID=FAKE_AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY=FAKE_AWS_SECRET_ACCESS_KEY \
+	--service-account-id ${SA_ID} \
+	--image ${CONTAINER_REGISTRY}/${REPOSITORY_NAME}:0.0.1
